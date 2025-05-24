233ca7f2b968e5fee73b69521096ef482bbded93
BetulAksan <betulaksan1992@gmail.com>
2025-05-24 09:49:12 +0300

    M200 - mbox

--- Ornek100/Form1.cs
+++ Ornek100/Form1.cs
@@ -9,7 +9,7 @@
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Günaydın");
+            MessageBox.Show("Günaydınlar");
         }
     }
 }
